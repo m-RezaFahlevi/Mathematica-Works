5d0b3049d0fdae97902109ddb33f0f3b46c55bb5
m-RezaFahlevi <57177960+m-RezaFahlevi@users.noreply.github.com>
2022-08-24 22:25:39 +0700

    range, mean, and var function are added

--- LearnRcpp/prog_cpp2.cpp
+++ LearnRcpp/prog_cpp2.cpp
@@ -84,6 +84,10 @@ int cumprodCpp(vector<int> v_vect) {
 	return prod;
 }
 
+// The purpose of this function is to transform a vector
+// numeric where the elements to be a cummulative maximum.
+// For example, v = (3, 2, 5), then the cumulative maximum
+// of v is (3, 3, 5).
 vector<int> cummaxCpp(vector<int> v_vect) {
 	int curr_thresh = std::numeric_limits<int>::min(); // a.k.a., -infinity
 	int n = v_vect.size();
@@ -97,6 +101,8 @@ vector<int> cummaxCpp(vector<int> v_vect) {
 	return v_vect;
 }
 
+// cumminCpp function is the converse of the cummax. For example,
+// if v = (3, 2, 5), then cummin of v is (3, 2, 2).
 vector<int> cumminCpp(vector<int> v_vect) {
 	int curr_thresh = std::numeric_limits<int>::max(); // a.k.a., infinity
 	int n = v_vect.size();
@@ -108,6 +114,56 @@ vector<int> cumminCpp(vector<int> v_vect) {
 			v_vect.at(curr_memb) = curr_thresh;
 	}
 	return v_vect;
+}
+
+int *fooptr(vector<int> v_vect) {
+	static int arrlocal[2];
+	arrlocal[0] = v_vect.at(0);
+	arrlocal[1] = v_vect.at(1);
+	return arrlocal;
+}
+
+int *rangeCpp(vector<int> v_vect) {
+	int min_thresh = std::numeric_limits<int>::min(); // a.k.a., -infinity
+	int max_thresh = std::numeric_limits<int>::max(); // a.k.a., +infinity
+	int *ptr = &min_thresh;
+
+	// linear (sequential) search is used
+	// to finding the maximum or the minimum
+	// members of v_vect
+	for (int nahlen: v_vect)
+		if (nahlen > *ptr)
+			*ptr = nahlen; // finding the max(v_vect)
+	ptr = &max_thresh;
+	for (int nahlen: v_vect)
+		if (nahlen < *ptr)
+			*ptr = nahlen; // finding the min(v_vect)
+
+	// max_thresh become the minimum value of v_vect,
+	// and min_thresh become the maximum value of v_vect
+	static int arrlocal[2] = {max_thresh, min_thresh};
+	return arrlocal;
+}
+
+float meanCpp(vector<int> v_vect) {
+	int n = v_vect.size();
+	double total = 0.0;
+	for (int nahlen: v_vect)
+		total += nahlen;
+	float measured = total / n;
+	return measured;
+}
+
+// variances could be defined as var(x) = mean(x^2) - mean^2(x)
+double varianceCpp(vector<int> v_vect) {
+	int n = v_vect.size();
+	vector<int> vsq_vect;
+	for (int i = 0; i < n; ++i) {
+		int vsq_memb = pow(v_vect.at(i), 2);
+		vsq_vect.push_back(vsq_memb);
+	}
+	double var_measured = meanCpp(vsq_vect) - pow(meanCpp(v_vect), 2);
+	return var_measured;
 }
 
 int main(void) {
@@ -132,4 +188,17 @@ int main(void) {
 	for (int curr_memb: cumminCpp(created_vect))
 		cout << curr_memb << " ";
 	cout << endl;
+
+	// function return an array;
+	int *ptrdec = fooptr(created_vect);
+	cout << ptrdec[0] << " " << ptrdec[1] << endl;
+	cout << *ptrdec << "\n";
+
+	// range function
+	int *obtain_range = rangeCpp(created_vect);
+	cout << obtain_range[0] << " " << obtain_range[1] << endl;
+
+	// var function
+	cout << "The variance of created vector is : ";
+	cout << varianceCpp(created_vect) << endl;
 }
