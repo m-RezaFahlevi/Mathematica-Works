c83c2d8d1a35307d049150e1c4f304218b67c2d9
m-RezaFahlevi <57177960+m-RezaFahlevi@users.noreply.github.com>
2022-08-26 22:21:32 +0700

    rleCpp is added for rcpp files

--- LearnRcpp/prog_rcpp5.cpp
+++ LearnRcpp/prog_rcpp5.cpp
@@ -19,6 +19,31 @@ int cumsumCppPtr(NumericVector v_vect) {
     return total;
 }
 
+// [[Rcpp::export]]
+List rleCpp(NumericVector v_vect) {
+    std::vector<int> v_value, v_length;
+    
+    // init first value
+    int prev_val = v_vect.at(0);
+    v_value.push_back(prev_val);
+    v_length.push_back(1);
+    
+    NumericVector::iterator ptrloc;
+    for (ptrloc = v_vect.begin() + 1; ptrloc != v_vect.end(); ++ptrloc) {
+        if (*ptrloc == prev_val) {
+            ++*v_length.rbegin();
+        } else {
+            v_value.push_back(*ptrloc);
+            v_length.push_back(1);
+            prev_val = *ptrloc;
+        }
+    }
+    
+    return List::create(
+        _["v_length"] = v_length,
+        _["v_value"] = v_value
+    );
+}
 
 // You can include R code blocks in C++ files processed with sourceCpp
 // (useful for testing and development). The R code will be automatically 
@@ -27,4 +52,5 @@ int cumsumCppPtr(NumericVector v_vect) {
 
 /*** R
 cumsumCppPtr(c(3, 2, 5))
+rleCpp(c(3, 2, 2, 2, 5, 5))
 */
