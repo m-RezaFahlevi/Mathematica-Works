a7a2565e4bbaa11efa09394ec21cd708518eccea
m-RezaFahlevi <57177960+m-RezaFahlevi@users.noreply.github.com>
2022-08-28 21:22:44 +0700

    unordered_set example from cplusplus.com

---
+++ LearnRcpp/unordered_setCpp.cpp
@@ -0,0 +1,23 @@
+// unordered_set::insert
+#include <iostream>
+#include <string>
+#include <array>
+#include <unordered_set>
+
+int main ()
+{
+		std::unordered_set<std::string> myset = {"yellow","green","blue"};
+		std::array<std::string,2> myarray = {"black","white"};
+		std::string mystring = "red";
+
+		myset.insert (mystring);                        // copy insertion
+		myset.insert (mystring+"dish");                 // move insertion
+		myset.insert (myarray.begin(), myarray.end());  // range insertion
+		myset.insert ( {"purple","orange"} );           // initializer list insertion
+
+		std::cout << "myset contains:";
+		for (const std::string& x: myset) std::cout << " " << x;
+		std::cout <<  std::endl;
+
+		return 0;
+}
