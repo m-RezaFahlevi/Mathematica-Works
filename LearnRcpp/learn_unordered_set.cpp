283175052a679b085e767ae2b218e5235bfcbf00
m-RezaFahlevi <57177960+m-RezaFahlevi@users.noreply.github.com>
2022-08-30 00:08:25 +0700

    Checking using pair of iterator and boolean

--- LearnRcpp/learn_unordered_set.cpp
+++ LearnRcpp/learn_unordered_set.cpp
@@ -3,15 +3,17 @@ using namespace std;
 
 int main(void) {
 	unordered_set<int> v_vect = {2, 3, 5, 7};
-	cout << *v_vect.insert(11).first << endl;
+	cout << v_vect.insert(11).second << endl;
 	unordered_set<int>::iterator ptrmain = v_vect.begin();
 	for (; ptrmain != v_vect.end(); ++ptrmain)
 		cout << *ptrmain << " ";
 	cout << endl;
-	
-	// choose member of v_vect
-	cout << *v_vect.find(2) << endl;
 
+	cout << "Display member of unordered_set directly\n";
+	for (const int &nahlen: v_vect)
+		cout << nahlen << " ";
+	cout << endl;
+	
 	// checking
 	cout << "checking whether a new element already inserted or not\n";
 	pair<unordered_set<int>::iterator, bool> ret = v_vect.insert(11);
